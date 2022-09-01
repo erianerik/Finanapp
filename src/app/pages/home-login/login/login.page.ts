10c4b54bcd4f0e3d75a39c17885b1c5f5beca98c
erianerik <erianerik@gmail.com>
2022-08-31 21:50:18 -0300

    chore: adicionando validação no login

--- src/app/pages/home-login/login/login.page.ts
+++ src/app/pages/home-login/login/login.page.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage-angular';
 import { Login } from 'src/app/model/Login';
+import { BroadcastService } from 'src/app/service/broadcast/broadcast.service';
 import { SessionStorageService } from 'src/app/service/session-storage/session-storage.service';
 import { UsuarioService } from 'src/app/service/usuario/usuario.service';
 
@@ -13,6 +14,7 @@ import { UsuarioService } from 'src/app/service/usuario/usuario.service';
 })
 export class LoginPage implements OnInit {
 
+  formErro = { senhaPossuiErro: false, emailPossuiErro: false, emailSenhaInvalido: false }
   login = new Login();
 
   constructor(
@@ -24,17 +26,29 @@ export class LoginPage implements OnInit {
   ngOnInit() { }
 
   async logForm(loginForm: any) {
+    if (this.validarFormulário()) { return; }
+    BroadcastService.toggleLoading();
     this._usuarioService.logarUsuario(loginForm.value).subscribe((result) => {
-      if (result.isAutenticado === false) { return; }
-      console.log(result);
-      
+      if (!result.autenticado) {
+        this.formErro.emailPossuiErro = this.formErro.senhaPossuiErro = false;
+        this.formErro.emailSenhaInvalido = true;
+        BroadcastService.toggleLoading();
+        return;
+      }
       this._sessionStorage.adicionarItemSession(result.idUsuario);
-      this.router.navigate(['profile']);
+      this.router.navigate(['/home-app']);
+      BroadcastService.toggleLoading();
     });
   }
 
+  validarFormulário() {
+    this.formErro.emailPossuiErro = this.login.email === '' ? true : false;
+    this.formErro.senhaPossuiErro = this.login.senha === '' ? true : false;
+    return this.formErro.emailPossuiErro || this.formErro.senhaPossuiErro;
+  }
+
   navigateRegister() {
-    this.router.navigate(['register-login']);
+    this.router.navigate(['/register-login']);
   }
 
 }
